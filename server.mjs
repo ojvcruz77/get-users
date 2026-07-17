a15c64fa7ac81987c0b50b6a1d46ad2a7e676cdd
ojvcruz77 <joaovictor.curriculos@gmail.com>
2026-07-16 21:04:46 -0300

    Feat: Adicionar interface para recurso de usuários

--- server.mjs
+++ server.mjs
@@ -1,6 +1,32 @@
-import http  from "node:http";
+import http, { STATUS_CODES } from "node:http";
+
+const users = [
+    {
+        nome: "João Victor",
+        idade: 26,
+        status: true
+    },
+    {
+        nome: "Ana",
+        idade: 32,
+        status: true
+    },
+    {
+        nome: "Luíza",
+        idade: 28,
+        status: false
+    }
+];
+
 
 http.createServer((req, res) => {
-    res.end('Hello Client');
+
+    if (req.url === "/users") {
+        res.writeHead(200, { 'content-type': 'application/json' });
+        res.end(JSON.stringify(users));
+    } else {
+        res.writeHead(404, { 'content-type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Pge not found' }));
+    }
 }).listen(3000);
 
