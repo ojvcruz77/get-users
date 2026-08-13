450bfcd15b8ac913bcd8184317f30b1aff32720a
ojvcruz77 <joaovictor.curriculos@gmail.com>
2026-08-13 19:39:20 -0300

    Atualizando codigos

---
+++ client/src/scripts/recovery.js
@@ -0,0 +1,31 @@
+const email = document.querySelector('input[type="email"]');
+const newPassword = document.querySelector('#new_password');
+const confirmNewPassword = document.querySelector('#confirm_new_password');
+const button = document.querySelector('button');
+
+button.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const emailValue = email.value;
+    const newPasswordValue = newPassword.value;
+    const confirmNewPasswordValue = confirmNewPassword.value;
+
+    if (emailValue !== sessionStorage.getItem('email'))
+        return alert('Email não registrado.');
+
+    if (
+        !emailValue ||
+        !newPasswordValue ||
+        !confirmNewPasswordValue
+    )
+        return alert('Preencha todos os campos.');
+
+    if (newPasswordValue !== confirmNewPasswordValue)
+        return alert('As senhas não coincidem, tente novamente.');
+
+     if (newPasswordValue === confirmNewPasswordValue)
+        sessionStorage.setItem('password', newPasswordValue);
+        alert('Senha alterada com sucesso.');
+    
+
+});
